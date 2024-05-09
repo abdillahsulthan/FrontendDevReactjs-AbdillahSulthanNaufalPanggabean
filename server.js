caddea92958da4e055b06aa4ffa524cfe1bc3414
Abdillah Sulthan Naufal Panggabean <abdillahsulthan12@gmail.com>
2024-05-10 01:27:31 +0700

    test: api

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+// const port = 3001;
 
 const corsOptions = {
   origin: 'https://restaurant-website-jet.vercel.app',
@@ -46,5 +46,5 @@ app.get('/restaurants/category/:category', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`API SERVER ON http://localhost:${port}`);
+    console.log(`API SERVER ON https://restaurant-website-jet.vercel.app/api`);
 });
